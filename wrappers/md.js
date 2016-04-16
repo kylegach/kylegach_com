9b7d4b063fef4d1e0a8997a739bdf96598479154
Kyle Gach <kyle.gach@gmail.com>
2016-04-15 22:23:46 -0600

    Changed wrapping <div> of Teaser component and markdown wrapper to <article>

--- wrappers/md.js
+++ wrappers/md.js
@@ -77,11 +77,13 @@ class MDWrapper extends Component {
     return (
       <DocumentTitle title={`${title} • ${config.siteTitle}`}>
         <main className="maxW-3 mx-auto">
-          <h1 className="h1 mb-0">{title}</h1>
-          { byline }
-          { intro }
-          { workHeader }
-          <div dangerouslySetInnerHTML={{ __html: page.data.body }} className="markdown mt-3" />
+          <article>
+            <h1 className="h1 mb-0">{title}</h1>
+            { byline }
+            { intro }
+            { workHeader }
+            <div dangerouslySetInnerHTML={{ __html: page.data.body }} className="markdown mt-3" />
+          </article>
         </main>
       </DocumentTitle>
     )
